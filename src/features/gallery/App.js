8760c84c7c8e00f1c294673f5caabfb9d7662d61
Jon Eric Esobedo <jonericescobedo@gmail.com>
2018-06-09 16:09:21 -0700

    chore: update folder structure

--- src/features/gallery/App.js
+++ src/features/gallery/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+// @flow
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+type Props = {};
+
+class App extends React.Component<Props> {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
