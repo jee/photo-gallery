e669fdcb7130e6d2957d6b96985e417f901e5c88
Jon Eric Esobedo <jonericescobedo@gmail.com>
2018-06-09 17:14:32 -0700

    fix: adjust path name in index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './features/gallery/App';
+import App from './features/App';
 import registerServiceWorker from './registerServiceWorker';
 
 const root = document.getElementById('root');
