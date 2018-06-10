0c982d2e73c9f1f2b0e799e87500ad30576f308e
Jon Eric Esobedo <jonericescobedo@gmail.com>
2018-06-10 00:27:23 -0700

    refactor: added url source to caption component

--- src/features/gallery/__tests__/components/caption.test.js
+++ src/features/gallery/__tests__/components/caption.test.js
@@ -6,15 +6,21 @@ import { shallow } from 'enzyme';
 describe('Caption', () => {
   describe('should render', () => {
     it('when the root component is loaded', () => {
-      const wrapper = shallow(<Caption caption={'Testing'} />);
+      const wrapper = shallow(<Caption caption={'Testing'} source={'http://via.placeholder.com/350x150'} />);
 
       expect(wrapper.find('.figure-caption').length).toBe(1);
     });
 
     it('with caption text when the caption prop is passed in', () => {
-      const wrapper = shallow(<Caption caption={'This is a caption!'} />);
-      const text = wrapper.find('.Caption').text();
+      const wrapper = shallow(<Caption caption={'This is a caption!'} source={'http://via.placeholder.com/350x150'} />);
+      const text = wrapper.find('.image-caption').text();
       expect(text).toEqual('This is a caption!');
+    });
+
+    it('with a link to the image source when the source prop is passed in', () => {
+      const wrapper = shallow(<Caption caption={'This is a caption!'} source={'http://via.placeholder.com/350x150'} />);
+      const imageSource = wrapper.find('a').props().href;
+      expect(imageSource).toEqual('http://via.placeholder.com/350x150');
     });
   });
 });
--- src/features/gallery/components/caption.js
+++ src/features/gallery/components/caption.js
@@ -3,13 +3,21 @@ import React from 'react';
 import '../styles/caption.css';
 
 type CaptionProps = {
-  caption: string
-}
+  caption: string,
+  source: string
+};
 
-const Caption = ({caption}: CaptionProps) => {
+const Caption = ({ caption, source }: CaptionProps) => {
   return (
     <div className="Caption figure">
-      <p className="figure-caption">{caption}</p>
+      <div className="figure-caption figure-caption-custom">
+        <p className="image-caption">{caption}</p>
+        <small className="text-muted image-source">
+          <a href={source} target="_blank" rel="noopener noreferrer">
+            &nbsp;source
+          </a>
+        </small>
+      </div>
     </div>
   );
 };
