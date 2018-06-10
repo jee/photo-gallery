daf8e6aa8ebe309618dc96293d8925602b535b2f
Jon Eric Esobedo <jonericescobedo@gmail.com>
2018-06-09 23:25:17 -0700

    feat: add photo component

---
+++ src/features/gallery/components/photo.js
@@ -0,0 +1,27 @@
+// @flow
+import React from 'react';
+import Caption from './caption';
+
+type PhotoProps = {
+  index: number,
+  photos: any
+};
+
+const Photo = ({ index, photos }: PhotoProps) => {
+  return (
+    <div className="Photo container-fluid">
+      <div className="row photo-box">
+        <figure className="col-lg-12">
+          <img
+            src={photos[index].urls.full}
+            className="img-fluid mx-auto d-block"
+            alt={photos[index].description}
+          />
+          <Caption caption={photos[index].description} />
+        </figure>
+      </div>
+    </div>
+  );
+};
+
+export default Photo;
