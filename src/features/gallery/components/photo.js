da2debbadbb5cee0a18c6067381786216dd3f295
Jon Eric Esobedo <jonericescobedo@gmail.com>
2018-06-10 19:08:18 -0700

    refactor: photo component and remove photo.css

--- src/features/gallery/components/photo.js
+++ src/features/gallery/components/photo.js
@@ -1,7 +1,6 @@
 // @flow
 import React from 'react';
 import Caption from './caption';
-import '../styles/photo.css';
 
 type PhotoProps = {
   index: number,
@@ -11,11 +10,11 @@ type PhotoProps = {
 const Photo = ({ index, photos }: PhotoProps) => {
   return (
     <div className="Photo container-fluid">
-      <div className="row photo-box">
+      <div className="row">
         <figure className="col">
           <img
             src={photos[index].urls.full}
-            className="img-fluid custom"
+            className="img-fluid"
             alt={photos[index].description}
           />
           <Caption caption={photos[index].description} source={photos[index].urls.full} />
